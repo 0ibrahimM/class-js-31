2c0d2fb42b1437fb6f18ddc8eb2ada889f6e99b3
0ibrahimM <meshkat271@gmail.com>
2024-11-13 11:28:04 -0800

    forEach

--- index.js
+++ index.js
@@ -1,94 +1,167 @@
 
 
 
-let marks=[
-    [33,23,51,55,65,88,89,99,95,99],
-    [87,95,59,55,45,40,60,30,32,44],
-    [38,39,35,93,63,53,43,73,78,75],
-    [49,76,48,69,54,15,42,94,54,75],
-]
+// let marks=[
+//     [33,23,51,55,65,88,89,99,95,99],
+//     [87,95,59,55,45,40,60,30,32,44],
+//     [38,39,35,93,63,53,43,73,78,75],
+//     [49,76,48,69,54,15,42,94,54,75],
+// ]
 
-console.log()
-console.log()
-console.log()
-console.log()
-console.log()
-console.log()
-console.log()
-console.log()
-console.log()
-console.log()
+// console.log()
+// console.log()
+// console.log()
+// console.log()
+// console.log()
+// console.log()
+// console.log()
+// console.log()
+// console.log()
+// console.log()
 
-for (i=0; i<=3; i++){
-if(i==0){
-    console.log("section-A")
-}
-else if(i==1){
-    console.log("section-B")
-}
-else if(i==2){
-    console.log("section-C")
-}
-else if(i==3){
-    console.log("section-D")
-}
-    for (y=0; y<=9; y++){
+// for (i=0; i<=3; i++){
+// if(i==0){
+//     console.log("section-A")
+// }
+// else if(i==1){
+//     console.log("section-B")
+// }
+// else if(i==2){
+//     console.log("section-C")
+// }
+// else if(i==3){
+//     console.log("section-D")
+// }
+//     for (y=0; y<=9; y++){
 
-        console.log(marks[i][y])
-if (marks[i][y] >= 80)
-console.log("A+")
-else if(marks[i][y] >= 70)
-    console.log("A")
-else if(marks[i][y]  >= 60)
-    console.log("A")
-else if(marks[i][y]>= 50)
-    console.log("B")
-else if(marks[i][y] >= 40)
-    console.log("C")
-else if(marks[i][y] >= 33)
-    console.log("D")
-    else{
-        console.log("the grade is: F ")
-    }
+//         console.log(marks[i][y])
+// if (marks[i][y] >= 80)
+// console.log("A+")
+// else if(marks[i][y] >= 70)
+//     console.log("A")
+// else if(marks[i][y]  >= 60)
+//     console.log("A")
+// else if(marks[i][y]>= 50)
+//     console.log("B")
+// else if(marks[i][y] >= 40)
+//     console.log("C")
+// else if(marks[i][y] >= 33)
+//     console.log("D")
+//     else{
+//         console.log("the grade is: F ")
+//     }
 
-    }
+//     }
        
-};
+// };
 
 
 
 
 
-function hello (){
-    let t =233;
-    let u =199;
-let result= t+u;
-console.log(result)
-}
-hello()
+// function hello (){
+//     let t =233;
+//     let u =199;
+// let result= t+u;
+// console.log(result)
+// }
+// hello()
 
-function hi (){
-    let w =233;
-    let q =199;
-let result= w-q;
-console.log(result)
-}
-hi()
+// function hi (){
+//     let w =233;
+//     let q =199;
+// let result= w-q;
+// console.log(result)
+// }
+// hi()
 
-function sun (){
-    let p =33;
-    let k =99;
-let result= p*k;
-console.log(result)
-}
-sun()
+// function sun (){
+//     let p =33;
+//     let k =99;
+// let result= p*k;
+// console.log(result)
+// }
+// sun()
 
 
-function why (){
-    let m =905;
-    let n =5;
-let result= m/n;
-console.log(result)
-}
-why()
+// function why (){
+//     let m =905;
+//     let n =5;
+// let result= m/n;
+// console.lforog(result)
+// }
+// why()
 
+
+let karks=[54,65,85,99,89,45,33,23,21,32,15,49,65,48,92,98,99,74,47,48,56,41,42,43,46,49,47,65,68,69,67,61,62,36]
+
+// (let i=0; i<=33;  i++){
+    
+
+//     // if (karks[i] >= 80)
+//     //     console.log("A+");
+//     //     else if(karks[i] >= 70){
+//     //         console.log( karks[i] +"=A")
+//     //     }
+           
+//     //     else if(karks[i] >= 60){
+//     //         console.log(karks[i] +"=A")
+//     //     }
+           
+//     //     else if(karks[i]>= 50){
+//     //         console.log(karks[i] +"=B")
+//     //     }
+           
+//     //     else if(karks[i] >= 40){
+//     //         console.log(karks[i] +"=C")
+//     //     }
+            
+//     //     else if(karks[i] >= 33){
+//     //         console.log(karks[i] +"=D")
+//     //     }
+          
+//     //        else{
+//     //             console.log(karks[i] +"=the grade is: F ");
+            
+
+
+
+//     //        }
+
+//         }
+karks.forEach((Number)=>{
+
+
+    
+
+    if (Number >= 80)
+        console.log("A+");
+        else if(Number >= 70){
+            console.log( Number +"=A")
+        }
+           
+        else if(Number >= 60){
+            console.log(Number +"=A")
+        }
+           
+        else if(Number>= 50){
+            console.log(Number +"=B")
+        }
+           
+        else if(Number >= 40){
+            console.log(Number +"=C")
+        }
+            
+        else if(Number >= 33){
+            console.log(Number +"=D")
+        }
+          
+           else{
+                console.log(Number +"=F ");
+            
+           }
+
+
+
+
+})
